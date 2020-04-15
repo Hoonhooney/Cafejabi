ab4678f20c1bacd1c8e729d6830e1d5b67e02eed
hoonhooney <xogns0520@ajou.ac.kr>
2020-04-16 00:22:55 +0900

    Main Activity에 네이버 지도 그리기(+ GPS 권한 추가)

--- app/src/main/java/com/example/cafejabi/MainActivity.java
+++ app/src/main/java/com/example/cafejabi/MainActivity.java
@@ -1,14 +1,48 @@
 package com.example.cafejabi;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.naver.maps.map.MapFragment;
+import com.naver.maps.map.NaverMap;
+import com.naver.maps.map.OnMapReadyCallback;
+import com.naver.maps.map.UiSettings;
+import com.naver.maps.map.util.FusedLocationSource;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+    private static final int ACCESS_LOCATION_PERMISSION_REQUEST_CODE = 100;
+    private FusedLocationSource locationSource;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        지도 보이기
+        MapFragment mapFragment = (MapFragment)getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+//    지도가 로딩된 후 작동(지도를 현재 위치로 맞춤)
+    @Override
+    public void onMapReady(@NonNull NaverMap naverMap) {
+        locationSource = new FusedLocationSource(this, ACCESS_LOCATION_PERMISSION_REQUEST_CODE);
+        naverMap.setLocationSource(locationSource);
+        UiSettings uiSettings = naverMap.getUiSettings();
+        uiSettings.setLocationButtonEnabled(true);
+    }
+
+//    위치 서비스 권한
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults){
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        switch(requestCode){
+            case ACCESS_LOCATION_PERMISSION_REQUEST_CODE:
+                locationSource.onRequestPermissionsResult(requestCode, permissions, grantResults);
+                return;
+        }
     }
 }

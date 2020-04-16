bfcea340b0f9620ec13058daeccb4e77a7ed748b
hoonhooney <xogns0520@ajou.ac.kr>
2020-04-16 19:12:47 +0900

    지도 기능 추가(초기 화면 최근 저장된 위치로, 줌 설정)

--- app/src/main/java/com/example/cafejabi/MainActivity.java
+++ app/src/main/java/com/example/cafejabi/MainActivity.java
@@ -3,8 +3,14 @@ package com.example.cafejabi;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
+import android.location.Location;
 import android.os.Bundle;
 
+import com.naver.maps.geometry.LatLng;
+import com.naver.maps.map.CameraAnimation;
+import com.naver.maps.map.CameraPosition;
+import com.naver.maps.map.CameraUpdate;
 import com.naver.maps.map.MapFragment;
 import com.naver.maps.map.NaverMap;
 import com.naver.maps.map.OnMapReadyCallback;
@@ -15,6 +21,8 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     private static final int ACCESS_LOCATION_PERMISSION_REQUEST_CODE = 100;
     private FusedLocationSource locationSource;
 
+    private SharedPreferences preferences;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,16 +30,30 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
 //        지도 보이기
         MapFragment mapFragment = (MapFragment)getSupportFragmentManager().findFragmentById(R.id.map);
+        assert mapFragment != null;
         mapFragment.getMapAsync(this);
     }
 
-//    지도가 로딩된 후 작동(지도를 현재 위치로 맞춤)
+//    지도가 로딩된 후 작동
     @Override
     public void onMapReady(@NonNull NaverMap naverMap) {
+
+        //최소, 최대 줌 설정
+        naverMap.setMinZoom(12.0);
+        naverMap.setMaxZoom(20.0);
+
+        // GPS 버튼 활성화
         locationSource = new FusedLocationSource(this, ACCESS_LOCATION_PERMISSION_REQUEST_CODE);
         naverMap.setLocationSource(locationSource);
         UiSettings uiSettings = naverMap.getUiSettings();
         uiSettings.setLocationButtonEnabled(true);
+
+        // 최신 위치로 이동
+        preferences = getSharedPreferences("LatestLocation", MODE_PRIVATE);
+        double lat = (double)preferences.getFloat("Latitude", 37.2788f);
+        double lon = (double)preferences.getFloat("Longitude", 127.0437f);
+
+        naverMap.setCameraPosition(new CameraPosition(new LatLng(lat, lon), 17, 20, 0));
     }
 
 //    위치 서비스 권한
@@ -44,5 +66,23 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
                 locationSource.onRequestPermissionsResult(requestCode, permissions, grantResults);
                 return;
         }
+    }
+
+//    종료할 때
+    @Override
+    public void onPause(){
+        super.onPause();
+
+        // 현위치 좌표 저장하기
+
+        Location lastLocation = locationSource.getLastLocation();
+
+        preferences = getSharedPreferences("LatestLocation", MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
+        if(lastLocation != null){
+            editor.putFloat("Latitude", (float)lastLocation.getLatitude());
+            editor.putFloat("Longitude", (float)lastLocation.getLongitude());
+        }
+        editor.apply();
     }
 }

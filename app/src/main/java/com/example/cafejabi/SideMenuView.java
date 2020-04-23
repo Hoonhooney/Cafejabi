b44b8d6dd41720ad4ce2126f25ccf731b1f183b5
hoonhooney <xogns0520@ajou.ac.kr>
2020-04-23 23:53:44 +0900

    로그인한 상태에서 SideMenuView에 사용자 닉네임, 스타일 보이기

--- app/src/main/java/com/example/cafejabi/SideMenuView.java
+++ app/src/main/java/com/example/cafejabi/SideMenuView.java
@@ -3,6 +3,7 @@ package com.example.cafejabi;
 import android.content.Context;
 import android.content.Intent;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -10,8 +11,18 @@ import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.RelativeLayout;
 import android.view.View;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
 import java.util.EventListener;
@@ -19,11 +30,12 @@ import java.util.List;
 import java.util.jar.Attributes;
 
 public class SideMenuView extends RelativeLayout implements View.OnClickListener {
+    private static final String TAG = "SideMenuView";
 
     public EventListener listener;
 
     private LinearLayout linearLayout_login, linearLayout_logout;
-
+    private TextView textView_username, textView_userstyle;
     private ListView listView_menu;
 
     public boolean isloggedin;
@@ -67,9 +79,34 @@ public class SideMenuView extends RelativeLayout implements View.OnClickListener
 
             findViewById(R.id.button_go_login).setOnClickListener(this);
         }else{
+            //로그인한 상태에 사용자 닉네임, 스타일 보이기
+
             linearLayout_login.setVisibility(VISIBLE);
             linearLayout_logout.setVisibility(GONE);
             list_menu.add("로그아웃");
+
+            textView_username = findViewById(R.id.textView_menu_userName);
+            textView_userstyle = findViewById(R.id.textView_menu_userStyle);
+
+            FirebaseAuth mAuth = FirebaseAuth.getInstance();
+            FirebaseUser user = mAuth.getCurrentUser();
+            FirebaseFirestore db = FirebaseFirestore.getInstance();
+            assert user != null;
+            DocumentReference ref = db.collection("users").document(user.getUid());
+            ref.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                @Override
+                public void onSuccess(DocumentSnapshot documentSnapshot) {
+                    UserInfo userInfo = documentSnapshot.toObject(UserInfo.class);
+                    assert userInfo != null;
+                    textView_username.setText(userInfo.getNickname());
+                    textView_userstyle.setText(userInfo.getStyle().toString());
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Log.e(TAG, "failed to get userInfo", e);
+                }
+            });
 
             findViewById(R.id.button_go_edit).setOnClickListener(this);
         }

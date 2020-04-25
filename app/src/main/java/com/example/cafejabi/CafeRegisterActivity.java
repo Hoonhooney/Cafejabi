6f623bfc692904726df568e356cde4ee6a89a7e3
hoonhooney <xogns0520@ajou.ac.kr>
2020-04-25 14:28:01 +0900

    CafeRegisterActivity(카페 등록 액티비티) 추가

---
+++ app/src/main/java/com/example/cafejabi/CafeRegisterActivity.java
@@ -0,0 +1,14 @@
+package com.example.cafejabi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CafeRegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+    }
+}

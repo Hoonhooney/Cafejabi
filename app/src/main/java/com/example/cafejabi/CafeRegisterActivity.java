f1327eb78e334878b10736891da4b26bb135a0c1
hoonhooney <xogns0520@ajou.ac.kr>
2020-04-25 21:01:30 +0900

    카페 등록 Activity 기능 추가 - 카페를 네이버 지역 검색 api로 위치, 주소, 이름 가져오기

--- app/src/main/java/com/example/cafejabi/CafeRegisterActivity.java
+++ app/src/main/java/com/example/cafejabi/CafeRegisterActivity.java
@@ -2,13 +2,158 @@ package com.example.cafejabi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.StrictMode;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
-public class CafeRegisterActivity extends AppCompatActivity {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.HashMap;
+import java.util.Map;
 
+public class CafeRegisterActivity extends AppCompatActivity implements View.OnClickListener {
+    private static final String TAG = "CafeRegisterActivity";
+    public static StringBuilder sb;
+
+    private EditText editText_cafe_name, editText_business_id, editText_cafe_address;
+
+    private String searchApiClientId, searchApiClientSecret;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        
+        setContentView(R.layout.activity_cafe_register);
+
+        init();
+    }
+
+    public void init(){
+
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+
+        editText_cafe_name = findViewById(R.id.editText_cafe_name);
+        editText_business_id = findViewById(R.id.editText_business_id);
+        editText_cafe_address = findViewById(R.id.editText_cafe_address);
+
+        //API key 가져오기
+        try{
+            ApplicationInfo ai = getPackageManager()
+                    .getApplicationInfo(getPackageName(), PackageManager.GET_META_DATA);
+            searchApiClientId = ai.metaData.getString("com.naver.search.CLIENT_ID");
+            searchApiClientSecret = ai.metaData.getString("com.naver.search.CLIENT_SECRET");
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        findViewById(R.id.button_check_cafe_address).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.button_check_cafe_address:
+                searchLocation();
+        }
+    }
+
+    private void searchLocation(){
+        Log.e(TAG, "searchLocation()");
+        String text = null;
+        int display = 2;
+
+        try {
+            text = URLEncoder.encode(editText_cafe_name.getText().toString(), "utf-8");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        String apiURL = "https://openapi.naver.com/v1/search/local?query=" + text;
+
+        Map<String, String> requestHeaders = new HashMap<>();
+        requestHeaders.put("X-Naver-Client-Id", searchApiClientId);
+        requestHeaders.put("X-Naver-Client-Secret", searchApiClientSecret);
+        String responseBody = get(apiURL, requestHeaders);
+
+        Log.d(TAG, responseBody);
+
+        Intent intent = new Intent(this, SelectLocationPopupActivity.class);
+        intent.putExtra("LOCATION_DATA",responseBody);
+        startActivityForResult(intent, 1);
+    }
+
+    private String get(String apiUrl, Map<String, String> requestHeaders){
+        HttpURLConnection con = connect(apiUrl);
+        try {
+            con.setRequestMethod("GET");
+            for(Map.Entry<String, String> header :requestHeaders.entrySet()) {
+                con.setRequestProperty(header.getKey(), header.getValue());
+            }
+
+            int responseCode = con.getResponseCode();
+            if (responseCode == HttpURLConnection.HTTP_OK) { // 정상 호출
+                return readBody(con.getInputStream());
+            } else { // 에러 발생
+                return readBody(con.getErrorStream());
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("API 요청과 응답 실패", e);
+        } finally {
+            con.disconnect();
+        }
+    }
+
+    private HttpURLConnection connect(String apiUrl){
+        try {
+            URL url = new URL(apiUrl);
+            return (HttpURLConnection) url.openConnection();
+        } catch (MalformedURLException e) {
+            throw new RuntimeException("API URL이 잘못되었습니다. : "+apiUrl, e);
+        } catch (IOException e) {
+            throw new RuntimeException("연결을 실패했습니다. : "+apiUrl, e);
+        }
+    }
+
+    private String readBody(InputStream body){
+        InputStreamReader streamReader = new InputStreamReader(body);
+
+        try (BufferedReader lineReader = new BufferedReader(streamReader)) {
+            StringBuilder responseBody = new StringBuilder();
+
+            String line;
+            while ((line = lineReader.readLine()) != null) {
+                responseBody.append(line);
+            }
+
+            return responseBody.toString();
+        } catch (IOException e) {
+            throw new RuntimeException("API 응답을 읽는데 실패했습니다.", e);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Log.e(TAG, "onActivityResult()");
+
+        if (requestCode == 1) {
+            if (resultCode == RESULT_OK) {
+                //데이터 받기
+                Log.e(TAG, data.getStringExtra("SELECTED_CAFE_NAME")+"");
+                editText_cafe_name.setText(data.getStringExtra("SELECTED_CAFE_NAME"));
+                editText_cafe_address.setText(data.getStringExtra("SELECTED_CAFE_ADDRESS"));
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/example/cafejabi/SelectLocationPopupActivity.java
@@ -0,0 +1,165 @@
+package com.example.cafejabi;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+public class SelectLocationPopupActivity extends Activity {
+    private static final String TAG = "LocationPopupActivity";
+
+    private ListView listView_cafe_locations;
+    private CafeListAdapter adapter;
+    private String data;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_select_location_popup);
+
+        Intent intent = getIntent();
+        data = intent.getStringExtra("LOCATION_DATA");
+
+        listView_cafe_locations = findViewById(R.id.listView_select_cafe_locations);
+        adapter = new CafeListAdapter();
+        listView_cafe_locations.setAdapter(adapter);
+
+        setDataToList();
+
+        listView_cafe_locations.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Log.e(TAG, "onItemClick()");
+                SimpleCafeInfo selectedCafe = (SimpleCafeInfo) adapter.getItem(i);
+                Intent intent = new Intent();
+                intent.putExtra("SELECTED_CAFE_NAME", selectedCafe.getCafeName());
+                intent.putExtra("SELECTED_CAFE_ADDRESS", selectedCafe.getCafeAddress());
+                intent.putExtra("SELECTED_CAFE_MAPX", selectedCafe.getMapx());
+                intent.putExtra("SELECTED_CAFE_MAPY", selectedCafe.getMapy());
+                setResult(RESULT_OK, intent);
+
+                finish();
+            }
+        });
+
+        if(adapter.isEmpty()){
+            findViewById(R.id.linearLayout_alert_empty_list).setVisibility(View.VISIBLE);
+            findViewById(R.id.button_alert_empty_list).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    finish();
+                }
+            });
+        }
+    }
+
+    private void setDataToList(){
+        Log.e(TAG, "setDataToList()");
+
+        try {
+            JSONObject jsonObject = new JSONObject(data);
+            JSONArray jsonArray = new JSONArray(jsonObject.getString("items"));
+            for (int i = 0; i < jsonArray.length(); i++) {
+                JSONObject object = jsonArray.getJSONObject(i);
+                Log.d(TAG, object.toString());
+                String title = object.getString("title").replace("<b>", "").replace("</b>","");
+                String address = object.getString("address");
+                String mapx = object.getString("mapx");
+                String mapy = object.getString("mapy");
+
+                adapter.addItem(new SimpleCafeInfo(title, address, mapx, mapy));
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public class CafeListAdapter extends BaseAdapter{
+        private ArrayList<SimpleCafeInfo> cafeList = new ArrayList<>();
+        public CafeListAdapter(){}
+
+        @Override
+        public int getCount() {
+            return cafeList.size();
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return cafeList.get(i);
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return i;
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+            Context context = viewGroup.getContext();
+
+            if(view == null){
+                LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                view = inflater.inflate(R.layout.item_locations, viewGroup, false);
+            }
+
+            TextView textView_cafeName = view.findViewById(R.id.textView_item_locations_cafe_name);
+            TextView textView_cafeAddress = view.findViewById(R.id.textView_item_locations_cafe_address);
+
+            SimpleCafeInfo cafe = cafeList.get(i);
+
+            textView_cafeName.setText(cafe.getCafeName());
+            textView_cafeAddress.setText(cafe.getCafeAddress());
+
+            return view;
+        }
+
+        public void addItem(SimpleCafeInfo cafe){
+            cafeList.add(cafe);
+        }
+    }
+
+    public static class SimpleCafeInfo{
+        private String cafeName;
+        private String cafeAddress;
+        private String mapx, mapy;
+
+        public SimpleCafeInfo(String cafeName, String cafeAddress, String mapx, String mapy) {
+            this.cafeName = cafeName;
+            this.cafeAddress = cafeAddress;
+            this.mapx = mapx;
+            this.mapy = mapy;
+        }
+
+        public String getCafeName() {
+            return cafeName;
+        }
+
+        public String getCafeAddress() {
+            return cafeAddress;
+        }
+
+        public String getMapx() {
+            return mapx;
+        }
+
+        public String getMapy() {
+            return mapy;
+        }
+    }
+}
